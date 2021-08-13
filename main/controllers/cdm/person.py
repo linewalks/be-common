e4e5e622cd7b7d1bb8dc41d9cf5e32caf24fab03
dongyun <Jaymel>
2021-08-13 18:57:08 +0900

    fix(visit): 불필요한 주석 제거, import order 수정, index endpoint 변경

--- main/controllers/cdm/person.py
+++ main/controllers/cdm/person.py
@@ -1,5 +1,5 @@
-from flask import Blueprint
 from main.models.utils import convert_query_to_response
+from flask import Blueprint
 from flask_apispec import marshal_with, doc
 from main import db
 from main.schema.cdm_schema import (
@@ -14,10 +14,12 @@ person_bp = Blueprint("person", __name__, url_prefix="/person")
 API_CATEGORY = "Person"
 
 
-@person_bp.route("/", methods=["GET"])
-@doc(tags=[API_CATEGORY],
-     summary="컬럼 설명",
-     description="환자 수 조회를 위한 파라미터를 설명합니다.")
+@person_bp.route("/index", methods=["GET"])
+@doc(
+    tags=[API_CATEGORY],
+    summary="컬럼 설명",
+    description="환자 수 조회를 위한 파라미터를 설명합니다."
+)
 def index():
   return ({
       "all": "모든 환자 수",
@@ -29,14 +31,19 @@ def index():
 
 
 @person_bp.route("/all", methods=["GET"])
-@marshal_with(PersonCount, description="""
-<pre>
-person_count: 전체 환자 수
-</pre>
-""")
-@doc(tags=[API_CATEGORY],
-     summary="전체 환자 수 조회",
-     description="전체 환자 수를 조회합니다.")
+@doc(
+    tags=[API_CATEGORY],
+    summary="전체 환자 수 조회",
+    description="전체 환자 수를 조회합니다."
+)
+@marshal_with(
+    PersonCount,
+    description="""
+    <pre>
+    person_count: 전체 환자 수
+    </pre>
+    """
+)
 def all_count():
   return {
       "person_count": db.session.query(Person).count()
@@ -44,18 +51,22 @@ def all_count():
 
 
 @person_bp.route("/gender", methods=["GET"])
-@doc(tags=[API_CATEGORY],
-     summary="성별 환자 수 조회",
-     description="성별 환자 수를 조회합니다.")
-@marshal_with(ResponseConceptPersonCount("person_list"),
-              description="""
-<pre>
-person_list: 환자 수 정보가 들어갈 리스트
-  .concept_id: 성별 Concept ID
-  .concept_name: 성별 Concept 이름
-  .person_count: 환자 수
-</pre>
-""")
+@doc(
+    tags=[API_CATEGORY],
+    summary="성별 환자 수 조회",
+    description="성별 환자 수를 조회합니다."
+)
+@marshal_with(
+    ResponseConceptPersonCount("person_list"),
+    description="""
+    <pre>
+    person_list: 환자 수 정보가 들어갈 리스트
+      .concept_id: 성별 Concept ID
+      .concept_name: 성별 Concept 이름
+      .person_count: 환자 수
+    </pre>
+    """
+)
 def gender_count():
   query = Person.person_gorup_by_condtion("gender_concept_id")
   return {
@@ -67,17 +78,22 @@ def gender_count():
 
 
 @person_bp.route("/race", methods=["GET"])
-@doc(tags=[API_CATEGORY],
-     summary="인종별 환자 수 조회",
-     description="인종별 환자 수를 조회합니다.")
-@marshal_with(ResponseConceptPersonCount("person_list"), description="""
-<pre>
-person_list: 환자 수 정보가 들어갈 리스트
-  .concept_id: 인종별 Concept ID
-  .concept_name: 인종별 Concept 이름
-  .person_count: 환자 수
-</pre>
-""")
+@doc(
+    tags=[API_CATEGORY],
+    summary="인종별 환자 수 조회",
+    description="인종별 환자 수를 조회합니다."
+)
+@marshal_with(
+    ResponseConceptPersonCount("person_list"),
+    description="""
+    <pre>
+    person_list: 환자 수 정보가 들어갈 리스트
+      .concept_id: 인종별 Concept ID
+      .concept_name: 인종별 Concept 이름
+      .person_count: 환자 수
+    </pre>
+    """
+)
 def race_count():
   query = Person.person_gorup_by_condtion("race_concept_id")
   return {
@@ -89,16 +105,21 @@ def race_count():
 
 
 @person_bp.route("/ethnicity", methods=["GET"])
-@doc(tags=[API_CATEGORY],
-     summary="민족별 환자 수 조회",
-     description="민족별 환자 수를 조회합니다.")
-@marshal_with(ResponseSourcePersonCount("person_list"), description="""
-<pre>
-person_list: 환자 수 정보가 들어갈 리스트
-  .source_value: 민족별 Source Value
-  .person_count: 환자 수
-</pre>
-""")
+@doc(
+    tags=[API_CATEGORY],
+    summary="민족별 환자 수 조회",
+    description="민족별 환자 수를 조회합니다."
+)
+@marshal_with(
+    ResponseSourcePersonCount("person_list"),
+    description="""
+    <pre>
+    person_list: 환자 수 정보가 들어갈 리스트
+      .source_value: 민족별 Source Value
+      .person_count: 환자 수
+    </pre>
+    """
+)
 def ethnicity_count():
   """
   ethnicitiy_concept_id의 모든 값이 0이다.
@@ -115,14 +136,19 @@ def ethnicity_count():
 
 
 @person_bp.route("/death", methods=["GET"])
-@doc(tags=[API_CATEGORY],
-     summary="사망 환자 수 조회",
-     description="사망 환자 수를 조회합니다.")
-@marshal_with(PersonCount, description="""
-<pre>
-person_count: 사망 환자 수
-</pre>
-""")
+@doc(
+    tags=[API_CATEGORY],
+    summary="사망 환자 수 조회",
+    description="사망 환자 수를 조회합니다."
+)
+@marshal_with(
+    PersonCount,
+    description="""
+    <pre>
+    person_count: 사망 환자 수
+    </pre>
+    """
+)
 def death_count():
   return {
       "person_count": db.session.query(Death).count()
